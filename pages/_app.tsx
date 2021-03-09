a9510df9f6aef6fc6824da213b898475599514d1
TheNomanAhmed <nomanahmed478@gmail.com>
2021-03-09 14:38:50 +0200

    wip: Authentication added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,18 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 // import { ApolloProvider } from "@apollo/client";
 // import { useApollo } from "src/apollo";
-// import { AuthProvider } from "src/auth/useAuth";
+import { AuthProvider } from "src/auth/useAuth";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <AuthProvider>
       <Head>
         <title>House Rental Application</title>
         <link rel="icon" href="/favicon.icot"></link>
       </Head>
 
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,14 @@
 import { FunctionComponent, ReactNode } from "react";
 import Link from "next/link";
-// import { useAuth } from "src/auth/useAuth";
+import { useAuth } from "src/auth/useAuth";
 
 interface IProps {
   main: ReactNode;
 }
 
 const Layout: FunctionComponent<IProps> = ({ main }) => {
-  const authenticated = false;
+  const { logout, authenticated } = useAuth();
+
   return (
     <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
       <nav className="bg-gray-800" style={{ height: "64px" }}>
@@ -16,7 +17,7 @@ const Layout: FunctionComponent<IProps> = ({ main }) => {
             <a>
               <img
                 src="/home-color.svg"
-                alt="Home House Logo"
+                alt="home house"
                 className="inline w-6"
               />
             </a>
@@ -26,14 +27,16 @@ const Layout: FunctionComponent<IProps> = ({ main }) => {
               <Link href="/houses/add">
                 <a>Add House</a>
               </Link>
-              <button onClick={() => {}}>Logout</button>
+              <button onClick={logout}>Logout</button>
             </>
           ) : (
-            <Link href="/auth">Login / Sign Up</Link>
+            <Link href="/auth">
+              <a>Login / Signup</a>
+            </Link>
           )}
         </div>
       </nav>
-      <main>{main}</main>
+      <main style={{ minHeight: "calc(100vh - 64px)" }}>{main}</main>
     </div>
   );
 };
