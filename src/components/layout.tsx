c95d0484af31ae24cbf9ba9d6090db91312f9bef
TheNomanAhmed <nomanahmed478@gmail.com>
2021-03-09 13:15:39 +0200

    wip: Added Layout structure

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,41 @@
-// import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+import { FunctionComponent, ReactNode } from "react";
+import Link from "next/link";
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+interface IProps {
+  main: ReactNode;
+}
+
+const Layout: FunctionComponent<IProps> = ({ main }) => {
+  const authenticated = false;
+  return (
+    <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
+      <nav className="bg-gray-800" style={{ height: "64px" }}>
+        <div className="px-6 flex items-center justify-between h-16">
+          <Link href="/">
+            <a>
+              <img
+                src="/home-color.svg"
+                alt="Home House Logo"
+                className="inline w-6"
+              />
+            </a>
+          </Link>
+          {authenticated ? (
+            <>
+              <Link href="/houses/add">
+                <a>Add House</a>
+              </Link>
+              <button onClick={() => {}}>Logout</button>
+            </>
+          ) : (
+            <Link href="/auth">Login / Sign Up</Link>
+          )}
+        </div>
+      </nav>
+      <main>{main}</main>
+    </div>
+  );
+};
+
+export default Layout;
