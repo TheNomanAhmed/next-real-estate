9610f9185e441ce8dad8753a7ff3844c6b8716b2
TheNomanAhmed <nomanahmed478@gmail.com>
2021-03-11 13:20:38 +0200

    wip: Added map bounds and also fixed different versions and interpolation error

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,20 +1,26 @@
 import { useRef, useState } from "react";
 import Link from "next/link";
 import { Image } from "cloudinary-react";
-import ReactMapGL, { Marker, Popup, MapRef } from "react-map-gl";
-import { ViewState } from "react-map-gl/index";
+import ReactMapGL, {
+  Marker,
+  Popup,
+  ViewState,
+  FlyToInterpolator,
+} from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-// import { useLocalState } from "src/utils/useLocalState";
+import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery_houses } from "src/generated/HousesQuery";
 // import { SearchBox } from "./searchBox";
 
-interface IProps {}
+interface IProps {
+  setDataBounds: (bounds: string) => void;
+}
 
-export default function Map({}: IProps) {
-  const mapRef = useRef<MapRef | null>(null);
-  const [viewport, setViewport] = useState<ViewState>({
-    latitude: 62.601,
-    longitude: 29.7636,
+export default function Map({ setDataBounds }: IProps) {
+  const mapRef = useRef<ReactMapGL | null>(null);
+  const [viewport, setViewport] = useLocalState<ViewState>("viewport", {
+    latitude: 43,
+    longitude: -79,
     zoom: 10,
   });
 
@@ -25,11 +31,25 @@ export default function Map({}: IProps) {
         width="100%"
         height="calc(100vh - 64px)"
         mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
-        onViewportChange={(nextViewport: any) => setViewport(nextViewport)}
+        transitionDuration={0} // add TransitionDuration
+        transitionInterpolator={new FlyToInterpolator()}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
         ref={(instance) => (mapRef.current = instance)}
         minZoom={5}
         maxZoom={15}
         mapStyle="mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef"
+        onLoad={() => {
+          if (mapRef.current) {
+            const bounds = mapRef.current.getMap().getBounds();
+            setDataBounds(JSON.stringify(bounds.toArray()));
+          }
+        }}
+        onInteractionStateChange={(extra) => {
+          if (!extra.isDragging && mapRef.current) {
+            const bounds = mapRef.current.getMap().getBounds();
+            setDataBounds(JSON.stringify(bounds.toArray()));
+          }
+        }}
       ></ReactMapGL>
     </div>
   );
