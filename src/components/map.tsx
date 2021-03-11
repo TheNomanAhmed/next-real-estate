28f6c176d4e627cac9dd9fe09b94538402d72b98
TheNomanAhmed <nomanahmed478@gmail.com>
2021-03-11 13:37:34 +0200

    wip: Search house on map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -10,7 +10,7 @@ import ReactMapGL, {
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useLocalState } from "src/utils/useLocalState";
 import { HousesQuery_houses } from "src/generated/HousesQuery";
-// import { SearchBox } from "./searchBox";
+import { SearchBox } from "./searchBox";
 
 interface IProps {
   setDataBounds: (bounds: string) => void;
@@ -54,6 +54,25 @@ export default function Map({ setDataBounds, houses, highlightedId }: IProps) {
           }
         }}
       >
+        <div className="absolute top-0 w-full z-10 p-4">
+          <SearchBox
+            defaultValue=""
+            onSelectAddress={(_address, latitude, longitude) => {
+              if (latitude && longitude) {
+                setViewport((old) => ({
+                  ...old,
+                  latitude,
+                  longitude,
+                  zoom: 12,
+                }));
+                if (mapRef.current) {
+                  const bounds = mapRef.current.getMap().getBounds();
+                  setDataBounds(JSON.stringify(bounds.toArray()));
+                }
+              }
+            }}
+          />
+        </div>
         {houses.map((house) => (
           <Marker
             key={house.id}
